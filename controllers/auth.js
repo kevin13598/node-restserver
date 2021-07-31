04fd3ec505cbe39f00cdff8f61f6b16215ecf245
Kevin <kevin.lam13@hotmail.com>
2021-07-31 13:53:57 -0600

    Google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response} = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -26,8 +27,9 @@ const login = async(req, res = response) => {
             });
         }
 
-        //Verificar contraseña
+        //Verificar contraseña con la de base datos
         const validPassword = bcryptjs.compareSync(password, usuario.password);
+        
         if(!validPassword){
             return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - estado - password'
@@ -50,11 +52,57 @@ const login = async(req, res = response) => {
             msg: 'Hable con el administrador'
         });
     }
+}
 
-    
+const googleSignIn = async(req, res = response) => {
+
+    const {id_token} = req.body;
+
+    try {
+
+        const {correo, nombre, img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario) {
+            //crear usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        if(!usuario.estado) {
+            res.status(401).json({
+                msg: 'Hable con el admdinsitrador, usuario bloqueado'
+            });
+        }
+
+        //Generar jwt
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+    }
+
 
 }
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
